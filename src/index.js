5a4557c97f0c10895bc1f388acbdb0038b919b67
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2021-10-06 00:01:45 +0300

    add main script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 import './sass/main.scss';
+import menu from './templates/menu-item.hbs';
+import menuList from './menu.json';
+
+// варианты тем
+const theme = {
+  light: 'light-theme',
+  dark: 'dark-theme',
+};
+
+const ref = {
+  menu: document.querySelector('.js-menu'),
+  themeSwitchToggle: document.querySelector('#theme-switch-toggle'),
+};
+
+// создать разметку меню
+const makeMenuList = list => menu(list);
+
+// вставить разметку на страничку
+ref.menu.insertAdjacentHTML('beforeend', makeMenuList(menuList));
+
+const toggleTheme = () => {
+  // переключить темы
+  switch (document.body.className) {
+    case theme.dark:
+      document.body.classList.remove(theme.dark);
+      document.body.classList.add(theme.light);
+      break;
+
+    default:
+      document.body.classList.remove(theme.light);
+      document.body.classList.add(theme.dark);
+      break;
+  }
+
+  // сохранить выбор темы
+  localStorage.setItem('theme', document.body.className);
+};
+
+// слушать переключатель тем
+ref.themeSwitchToggle.addEventListener('change', toggleTheme);
+
+// прочитать сохраненную тему, если есть
+if (localStorage.getItem('theme')) document.body.classList.add(localStorage.getItem('theme'));
+
+// проверить корректность положения переключателя тем
+if (localStorage.getItem('theme') === theme.dark)
+  ref.themeSwitchToggle.setAttribute('checked', true);
